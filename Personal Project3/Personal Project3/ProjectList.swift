15ddfcac9b96670319a52b94a82ea380c51cf602
Preeti Patel <preeti@preetis-macbook-pro.local>
2016-03-27 09:34:34 -0400

    Created a new ProjectList class and projectList object that was factored out of ProjectTableViewController.swift.  This will make it easier to access the list of projects from multiple view controllers.  A stub sort() method was added but not implemented yet.

---
+++ Personal Project3/Personal Project3/ProjectList.swift
@@ -0,0 +1,78 @@
+//
+//  ProjectList.swift
+//  Personal Project3
+//
+//  Created by Preeti Patel on 2016-03-26.
+//  Copyright © 2016 Nick Patel. All rights reserved.
+//
+
+import Foundation
+
+class ProjectList {
+
+    // MARK: Properties
+    var projects = [Project]()
+
+    // MARK: Intitialization
+    init() {
+        load()
+    }
+
+    // MARK: Add Project
+    func add(project: Project) {
+        projects.append(project)
+        sort()
+        save()
+    }
+
+    // MARK: Remove Project
+    func remove(index: Int) {
+        projects.removeAtIndex(index)
+        save()
+    }
+
+    // MARK: Update Project
+    func update(index: Int, project: Project) {
+        projects[index] = project;
+        sort()
+        save()
+    }
+
+    // MARK: Get Project
+    func get(index: Int) -> Project {
+        return projects[index]
+    }
+
+    // MARK: Indiciate the number of projects
+    func count() -> Int {
+        return projects.count
+    }
+
+    // MARK: Sort Entries
+    func sort() {
+        // Sort in this order:
+        // 1. For all projects that have already started, sort by due date (earliest to latest).
+        // 2. For all projects that haven't started yet, sort by start date (earliest to latest).
+    }
+
+    // MARK: Save
+    func save() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(projects, toFile: Project.ArchiveURL.path!)
+        if !isSuccessfulSave {
+            print("Failed to save meals...")
+        }
+    }
+
+    // MARK: Load
+    func load() {
+        // Load any saved projects.
+        if let savedProjects = NSKeyedUnarchiver.unarchiveObjectWithFile(Project.ArchiveURL.path!) as? [Project] {
+            projects += savedProjects
+        } else {
+            print("No saved projects found.")
+        }
+    }
+    
+}
+
+var projectList = ProjectList()
--- Personal Project3/Personal Project3/ProjectTableViewController.swift
+++ Personal Project3/Personal Project3/ProjectTableViewController.swift
@@ -10,15 +10,12 @@ import UIKit
 
 class ProjectTableviewController: UITableViewController {
 
-    // MARK: Properties
-    var projects = [Project]()
-
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return projects.count
+        return projectList.count()
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -26,7 +23,7 @@ class ProjectTableviewController: UITableViewController {
         let cellIdentifier = "ProjectTableViewCell"
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ProjectTableViewCell
         // Fetches the appropriate meal for the data source layout.
-        let project = projects[indexPath.row]
+        let project = projectList.get(indexPath.row)
         cell.nameLabel.text = project.name
         cell.totalDaysLabel.text = String(NSCalendar.currentCalendar().components(NSCalendarUnit.Day, fromDate: project.startDate, toDate: project.dueDate, options: NSCalendarOptions()).day) + " total days"
         let now = NSDate()
@@ -38,8 +35,7 @@ class ProjectTableviewController: UITableViewController {
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
-            projects.removeAtIndex(indexPath.row)
-            saveProjects()
+            projectList.remove(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
@@ -56,12 +52,6 @@ class ProjectTableviewController: UITableViewController {
         super.viewDidLoad()
         // Use the edit button item provided by the table view controller.
         navigationItem.leftBarButtonItem = editButtonItem()
-        // Load any saved projects, otherwise load sample data.
-        if let savedProjects = loadProjects() {
-            projects += savedProjects
-        } else {
-            print("No saved projects found.")
-        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -73,16 +63,14 @@ class ProjectTableviewController: UITableViewController {
         if let sourceViewController = sender.sourceViewController as? ProjectViewController, project = sourceViewController.project {
             if let selectedIndexPath = tableView.indexPathForSelectedRow {
                 // Update an existing project.
-                projects[selectedIndexPath.row] = project
+                projectList.update(selectedIndexPath.row, project: project)
                 tableView.reloadRowsAtIndexPaths([selectedIndexPath], withRowAnimation: .None)
             } else {
                 // Add a new project.
-                let newIndexPath = NSIndexPath(forRow: projects.count, inSection: 0)
-                projects.append(project)
+                let newIndexPath = NSIndexPath(forRow: projectList.count(), inSection: 0)
+                projectList.add(project)
                 tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
             }
-            // Save the projects.
-            saveProjects()
         }
     }
 
@@ -92,24 +80,11 @@ class ProjectTableviewController: UITableViewController {
             // Get the cell that generated this segue.
             if let selectedProjectCell = sender as? ProjectTableViewCell {
                 let indexPath = tableView.indexPathForCell(selectedProjectCell)!
-                let selectedProject = projects[indexPath.row]
-                projectDetailViewController.project = selectedProject
+                projectDetailViewController.project = projectList.get(indexPath.row)
             }
         } else if segue.identifier == "AddItem" {
             print("Adding new project.")
         }
-    }
-
-    // MARK: NSCoding
-    func saveProjects() {
-        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(projects, toFile: Project.ArchiveURL.path!)
-        if !isSuccessfulSave {
-            print("Failed to save meals...")
-        }
-    }
-
-    func loadProjects() -> [Project]? {
-        return NSKeyedUnarchiver.unarchiveObjectWithFile(Project.ArchiveURL.path!) as? [Project]
     }
 
 }
