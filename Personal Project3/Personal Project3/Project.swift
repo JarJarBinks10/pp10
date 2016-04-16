6d52b8a336b112fb525dfed863b38182de573f38
Preeti Patel <preeti@preetis-macbook-pro.local>
2016-04-16 12:10:20 -0400

    Added a notes property to the Project class.  And simplified the addTask() method in the Task class.

--- Personal Project3/Personal Project3/Project.swift
+++ Personal Project3/Personal Project3/Project.swift
@@ -16,6 +16,7 @@ class Project: NSObject, NSCoding {
         static let nameKey = "name"
         static let startDateKey = "startDate"
         static let dueDateKey = "dueDate"
+        static let notesKey = "notes"
         static let tasksKey = "tasks"
     }
 
@@ -23,6 +24,7 @@ class Project: NSObject, NSCoding {
     var name: String
     var startDate: NSDate
     var dueDate: NSDate
+    var notes: String
     var tasks = [Task]()
 
     // MARK: Archiving Paths
@@ -35,11 +37,21 @@ class Project: NSObject, NSCoding {
         self.name = name
         self.startDate = startDate
         self.dueDate = dueDate
+        self.notes = ""
         super.init()
         // Initialization should fail if there is no name or if dueDate is before startDate.
         if name.isEmpty || startDate.compare(dueDate) != .OrderedAscending {
             return nil
         }
+    }
+
+    convenience init?(name: String, startDate: NSDate, dueDate: NSDate, notes: String) {
+        self.init(name: name, startDate: startDate, dueDate: dueDate)
+        self.notes = notes
+    }
+
+    func updateNotes(notes: String) {
+        self.notes = notes
     }
 
     func addTask(name: String, startDate: NSDate, dueDate: NSDate) {
@@ -51,6 +63,7 @@ class Project: NSObject, NSCoding {
         aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
         aCoder.encodeObject(startDate, forKey: PropertyKey.startDateKey)
         aCoder.encodeObject(dueDate, forKey: PropertyKey.dueDateKey)
+        aCoder.encodeObject(notes, forKey: PropertyKey.notesKey)
         //aCoder.encodeObject(tasks, forKey: PropertyKey.tasksKey)
     }
 
@@ -58,9 +71,10 @@ class Project: NSObject, NSCoding {
         let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
         let startDate = aDecoder.decodeObjectForKey(PropertyKey.startDateKey) as! NSDate
         let dueDate = aDecoder.decodeObjectForKey(PropertyKey.dueDateKey) as! NSDate
+        let notes = aDecoder.decodeObjectForKey(PropertyKey.notesKey) as! String
         //let tasks = aDecoder.decodeObjectForKey(PropertyKey.tasksKey) as! [Task]
         // Must call designated initializer
-        self.init(name: name, startDate: startDate, dueDate: dueDate)
+        self.init(name: name, startDate: startDate, dueDate: dueDate, notes: notes)
     }
 
 }
