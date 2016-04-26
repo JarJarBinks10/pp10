05afb4f83272c5ca0eeb6d668cd22a9a3309a3aa
Nick Patel <nickpatel@rakeshs-macbook-air.local>
2016-04-26 07:39:30 -0400

    began writing code for the task section.

---
+++ Personal Project3/Personal Project3/Notesviewcontroller.swift
@@ -0,0 +1,26 @@
+//
+//  Notesviewcontroller.swift
+//  Personal Project3
+//
+//  Created by Nick Patel on 2016-04-24.
+//  Copyright © 2016 Nick Patel. All rights reserved.
+//
+
+import UIKit
+
+class Notesviewcontroller: UIViewController {
+    
+    // MARK: Properties 
+    
+    @IBOutlet weak var nameTextView: UITextView!
+    @IBAction func SaveButton(sender: UIBarButtonItem) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
---
+++ Personal Project3/Personal Project3/TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewController.swift
+//  Personal Project3
+//
+//  Created by Nick Patel on 2016-04-24.
+//  Copyright © 2016 Nick Patel. All rights reserved.
+//
+
+import Cocoa
+
+class TableViewController: UITableViewController {
+
+}
