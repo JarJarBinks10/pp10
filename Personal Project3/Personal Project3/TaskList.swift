10543d2fe51077e4f6a6b3b8acc4c3e7dd681c84
Preeti Patel <preeti@preetis-macbook-pro.local>
2016-04-26 22:39:40 -0400

    Resolved the build errors related the TaskList.swift file.  The problem seems to have been that the TaskList.swift file was in the wrong directory. This can be seen in the project.pbxproj file.  In the old file, line 54 referred to ../TaskList.swift.  In the new file line 56 refers to TaskList.swift.

--- Personal Project3/Personal Project3/TaskList.swift
+++ Personal Project3/Personal Project3/TaskList.swift
@@ -1,75 +1,75 @@
 //
-//  TaskList.Swift
+//  TaskList.swift
 //  Personal Project3
 //
-//  Created by Nick Patel on 2016-04-26.
+//  Created by Preeti Patel on 2016-04-26.
 //  Copyright © 2016 Nick Patel. All rights reserved.
 //
 
 import Foundation
 
 class TaskList {
-    
+
     // MARK: Properties
     var tasks = NSMutableArray()
-    
+
     // MARK: Intitialization
     init() {
         //load()
     }
-    
+
     // MARK: Add Task
     func add(task: Task) {
         tasks.addObject(task)
         sort()
         //save()
     }
-    
+
     // MARK: Remove Task
     func remove(index: Int) {
         tasks.removeObjectAtIndex(index)
         //save()
     }
-    
+
     // MARK: Update Task
     func update(index: Int, task: Task) {
         tasks.replaceObjectAtIndex(index, withObject: task)
         sort()
         //save()
     }
-    
+
     // MARK: Get Task
     func getTask(index: Int) -> Task {
         return tasks[index] as! Task
     }
-    
-    // MARK: Indiciate the number of task
+
+    // MARK: Indiciate the number of tasks
     func count() -> Int {
         return tasks.count
     }
-    
+
     // MARK: Sort Entries
     func sort() {
         tasks.sortUsingFunction(compareTasks, context: UnsafeMutablePointer<Void>(bitPattern: 0))
     }
-    
+
     /*
-     // MARK: Archiving Paths
-     let ArchiveURL = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!.URLByAppendingPathComponent("projects")
-     
-     // MARK: Save
-     func save() {
-     NSKeyedArchiver.archiveRootObject(tasks, toFile: ArchiveURL.path!)
-     }
-     
-     // MARK: Load
-     func load() {
-     // Load any saved projects.
-     if let savedProjects = NSKeyedUnarchiver.unarchiveObjectWithFile(ArchiveURL.path!) as? NSMutableArray {
-     tasks.addObjectsFromArray(savedProjects as [AnyObject])
-     }
-     }
-     */
+    // MARK: Archiving Paths
+    let ArchiveURL = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!.URLByAppendingPathComponent("tasks")
+
+    // MARK: Save
+    func save() {
+        NSKeyedArchiver.archiveRootObject(tasks, toFile: ArchiveURL.path!)
+    }
+
+    // MARK: Load
+    func load() {
+        // Load any saved tasks.
+        if let savedTasks = NSKeyedUnarchiver.unarchiveObjectWithFile(ArchiveURL.path!) as? NSMutableArray {
+            tasks.addObjectsFromArray(savedTasks as [AnyObject])
+        }
+    }
+    */
     
 }
 
